e10498f2c68d9a772311b8180667ccc291aa5328
juanescorreap <juanestebancope@outlook.com>
2022-05-20 16:56:27 -0500

    task 10

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,16 @@
+function countPrimeNumbers() {
+    let counter = 0;
+    for (let i = 2; i <= 100; i++) {
+        if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+const start_time = performance.now();
+for(let j = 0; j < 100; j++) {
+    countPrimeNumbers();
+}
+const finish_time = performance.now();
+const process_time = finish_time - start_time;
+console.log(`Execution time of calculating prime numbers 100 times was ${process_time} milliseconds.`);
